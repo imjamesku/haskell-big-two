a231bbee4c36739c80e4c8c9c88147e95af223ef
James Ku <jameskutw@gmail.com>
2023-11-18 16:46:15 +0800

    Complete core functions

--- src/Core.hs
+++ src/Core.hs
@@ -1,7 +1,10 @@
-module Core (createDeck, shuffle) where
+module Core (createDeck, shuffle, compareCombinations, deal) where
 
 import Control.Monad
 import Data.Array.IO
+import Data.List (foldl', maximumBy)
+import qualified Data.Map.Strict as Map
+import Data.Ord (comparing)
 import System.Random
 
 data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Eq, Ord, Enum, Show)
@@ -10,9 +13,17 @@ data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Ord, Enum, Show)
 
 data Card = Card {rank :: Rank, suit :: Suit} deriving (Eq, Show, Ord)
 
+data FiveCards = FiveCards Card Card Card Card Card deriving (Eq, Show, Ord)
+
+data TwoCards = TwoCards Card Card deriving (Eq, Show, Ord)
+
 type Deck = [Card]
 
 type Hand = [Card]
+
+type Trick = [Card]
+
+data Combination = Pair TwoCards | Straight FiveCards | FullHouse FiveCards | FourOfAKind FiveCards | StraightFlush FiveCards deriving (Eq, Show)
 
 createDeck :: Deck
 createDeck = [Card r s | r <- [Ace .. King], s <- [Clubs .. Spades]]
@@ -40,3 +51,28 @@ deal deck =
       (hand3, deck''') = splitAt 13 deck''
       (hand4, _) = splitAt 13 deck'''
    in [hand1, hand2, hand3, hand4]
+
+compareCombinations :: Combination -> Combination -> Maybe Ordering
+compareCombinations comb1 comb2 = case (comb1, comb2) of
+  (Pair t1, Pair t2) -> Just $ comparePair t1 t2
+  (Straight f1, Straight f2) -> Just $ compareStraight f1 f2
+  (FullHouse f1, FullHouse f2) -> Just $ compareFullHouse f1 f2
+  --   (FourOfAKind _, FourOfAKind _) -> compareFourOfAKind comb1 comb2
+  --   (StraightFlush _, StraightFlush _) -> compareStraightFlush comb1 comb2
+  _ -> Nothing
+
+comparePair :: TwoCards -> TwoCards -> Ordering
+comparePair (TwoCards c1 c2) (TwoCards c3 c4) = Prelude.compare (max c1 c2) (max c3 c4)
+
+compareStraight :: FiveCards -> FiveCards -> Ordering
+compareStraight (FiveCards c1 c2 c3 c4 c5) (FiveCards c6 c7 c8 c9 c10) = Prelude.compare (maximum [c1, c2, c3, c4, c5]) (maximum [c6, c7, c8, c9, c10])
+
+compareFullHouse :: FiveCards -> FiveCards -> Ordering
+compareFullHouse f1 f2 = Prelude.compare (findRank f1) (findRank f2)
+  where
+    findRank :: FiveCards -> Rank
+    findRank (FiveCards c1 c2 c3 c4 c5) = findMaxKey . foldl' updateMap Map.empty . map rank $ [c1, c2, c3, c4, c5]
+    updateMap :: Map.Map Rank Int -> Rank -> Map.Map Rank Int
+    updateMap m x = Map.insertWith (+) x 1 m
+    findMaxKey :: (Ord v) => Map.Map k v -> k
+    findMaxKey = fst . maximumBy (comparing snd) . Map.toList
